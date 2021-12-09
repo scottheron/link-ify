8fc4c99409df86a10892d5ba6c7e2781a9b1bb88
scottheron <scott.heron@outlook.com>
2021-12-09 15:01:02 -0800

    update

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-mongoose.connect(process.env.ORMONGO_SHORTENER_URL ||'mongodb://localhost/urlShortener', {
+var username = process.env.DB_USER
+var password = process.env.DB_PASSWORD
+var hosts = 'iad2-c16-1.mongo.objectrocket.com:54679,iad2-c16-2.mongo.objectrocket.com:54679,iad2-c16-0.mongo.objectrocket.com:54679'
+var database = 'urlShortener'
+var options = '?replicaSet=95f7e1f12bec43dc965e77f90fc3a992'
+var connectionString = process.env.DB_USER ? 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options : 'mongodb://localhost/urlShortener'
+
+mongoose.connect(connectionString ||'mongodb://localhost/urlShortener', {
     useNewUrlParser: true, useUnifiedTopology: true
-})
+});
+
+// mongoose.connect(process.env.ORMONGO_SHORTENER_URL ||'mongodb://localhost/urlShortener', {
+//     useNewUrlParser: true, useUnifiedTopology: true
+// })
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
