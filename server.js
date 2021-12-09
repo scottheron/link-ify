51b45b2907148ddf723c3113abe250abb9cfe2b3
scottheron <scott.heron@outlook.com>
2021-12-09 15:06:38 -0800

    added param

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var username = process.env.DB_USER
 var password = process.env.DB_PASSWORD
 var hosts = 'iad2-c16-1.mongo.objectrocket.com:54679,iad2-c16-2.mongo.objectrocket.com:54679,iad2-c16-0.mongo.objectrocket.com:54679'
 var database = 'urlShortener'
-var options = '?replicaSet=95f7e1f12bec43dc965e77f90fc3a992'
+var options = '?replicaSet=95f7e1f12bec43dc965e77f90fc3a992&retryWrites=false'
 var connectionString = process.env.DB_USER ? 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options : 'mongodb://localhost/urlShortener'
 
 mongoose.connect(connectionString ||'mongodb://localhost/urlShortener', {
